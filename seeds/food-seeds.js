50fbf32ca1985f0dfbdba704cabd35cc06e32ef5
James Kelly <jk377y@gmail.com>
2023-02-06 14:16:07 -0600

    added some mock seed data

--- seeds/food-seeds.js
+++ seeds/food-seeds.js
@@ -1,13 +1,24 @@
-const seedFood = require('./food-seeds.js');// Import the "seedFood" array from the food seeds file
-const sequelize = require('../config/connection.js');// Import the "sequelize" connection object from the connection configuration file
+const Food = require('./Food');// Import the "Food" model
 
-const seedDb = async () => {// Define an asynchronous function to seed the database
-    await sequelize.sync({ force: true });// Synchronize the database, dropping any existing tables and recreating them
-    console.log('\n ===== DATABASE SYNCED =====\n');// Log a message to the console
-    
-    await seedFood();// Seed the database with the "seedFood" array
-    console.log('\n ===== FOOD SEEDED =====\n');// Log a message to the console
-    
-    process.exit(0);// Exit the process
-};
-seedDb();// Call the "seedDb" function
+const foodData = [
+    {
+        name: 'apple',
+        calories: 95,
+        price: 0.99
+    },
+    {
+        name: 'banana',
+        calories: 105,
+        price: 0.79
+    },
+    {
+        name: 'carrot',
+        calories: 41,
+        price: 0.59
+    },
+    {
+        name: 'chicken breast',
+        calories: 165,
+        price: 3.99
+    },
+]
