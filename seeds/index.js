458789fbd2018fea973a0e954a5549dac6af653c
James Kelly <jk377y@gmail.com>
2023-02-12 21:41:51 -0600

    rebuilt seed function to seed the category and food mock data files

--- seeds/index.js
+++ seeds/index.js
@@ -1,18 +1,22 @@
-const seedFood = require('./food-seeds.js');// Import the "seedFood" array from the food seeds file
-const sequelize = require('../config/connection.js');// Import the "sequelize" connection object from the connection configuration file
+const sequelize = require('../config/connection');// import the sequelize connection from the config folder
+const seedFood = require('./foodData');// import the seedFood function from the foodData.js file
+const seedCategory = require('./categoryData');// import the seedCategory function from the categoryData.js file
 
-const seedDb = async () => {// Define an asynchronous function to seed the database
-    try {
-        await sequelize.sync({ force: true });// Synchronize the database, dropping any existing tables and recreating them
-        console.log('\n ===== DATABASE SYNCED =====\n');// Log a message to the console
+const seedAll = async () => {// Define an asynchronous function to seed the database
+  try {
+      await sequelize.sync({ force: true });// Synchronize the database, dropping any existing tables and recreating them
+      console.log('\n ===== DATABASE SYNCED =====\n');// Log a message to the console
 
-        await seedFood();// Seed the database with the "seedFood" array
-        console.log('\n ===== FOOD SEEDED =====\n');// Log a message to the console
+      await seedCategory();// Seed the database with the "seedCategory" array
+      console.log('\n ===== CATEGORY SEEDED =====\n');// Log a message to the console
 
-        process.exit(0);// Exit the process
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-        }
+      await seedFood();// Seed the database with the "seedFood" array
+      console.log('\n ===== FOOD SEEDED =====\n');// Log a message to the console
+      
+      process.exit(0);// Exit the process
+  } catch (error) {
+      console.error(error);
+      process.exit(1);
+      }
 };
-seedDb();// Call the "seedDb" function
+seedAll();// call the seedAll function to seed the database with data
