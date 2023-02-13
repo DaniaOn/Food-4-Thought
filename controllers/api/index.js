05de3f7188f1a4c95b9da2dc146722dc29dcf5e9
James Kelly <jk377y@gmail.com>
2023-02-12 22:08:07 -0600

    fixed input css file because tailwind was not building correctly

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();// Import the express package and use the Router() method to create a new router object
+const foodRoutes = require('./food-routes');// Import the food routes from the food-routes.js file
+const userRoutes = require('./user-routes');// Import the user routes from the user-routes.js file
 
-const userRoutes = require('../userRoutes');// Import the userRoutes file
-const foodRoutes = require('./foodRoutes');// Import the foodRoutes file
-
-router.use('/user', userRoutes);// Use the use() method to add the userRoutes file to the router object
-router.use('/food', foodRoutes);// Use the use() method to add the foodRoutes file to the router object
+router.use('/food', foodRoutes);// Use the food routes when the user visits any route that starts with /foods (localhost:3001/foods)
+router.use('/users', userRoutes);// Use the user routes when the user visits any route that starts with /users (localhost:3001/users)
 
 module.exports = router;// Export the router object to be used in the server.js file
