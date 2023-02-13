4f4c4b29c71dfd2c04257166e7def4ed5963809d
James Kelly <jk377y@gmail.com>
2023-02-12 21:30:32 -0600

    moved user-routes; rebuilt homeroutes; rebuilt index routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+const router = require('express').Router();// Import the express package and use the Router() method to create a new router object
+const apiRoutes = require('./api');// Import the api routes from the api folder
+const homeRoutes = require('./home-routes.js');// Import the home routes from the home-routes.js file
+const foodRoutes = require('./api/food-routes');// Import the food routes from the food-routes.js file
+const userRoutes = require('./api/user-routes');// Import the user routes from the user-routes.js file
 
-const foodroutes = require('./api/foodRoutes.js');
-const loginRoutes = require('./loginRoutes.js');
-// const userRoutes = require('./userRoutes.js');
+router.use('/', homeRoutes);// Use the home routes when the user visits the homepage (localhost:3001/)
+router.use('/api', apiRoutes);// Use the api routes when the user visits any route that starts with /api (localhost:3001/api)
+router.use('/food', foodRoutes);// Use the food routes when the user visits any route that starts with /api/food (localhost:3001/api/food)
+router.use('/api/users', userRoutes);// Use the user routes when the user visits any route that starts with /api/users (localhost:3001/api/users)
 
-// router.use('/', foodroutes);
-router.use('/login', loginRoutes);
-// router.use('/user', userRoutes);
-
-module.exports = router;
+module.exports = router;// Export the router object to be used in the server.js file
