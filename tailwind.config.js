a118ee13e7e207dbe579a8523f8df29085d86d74
DaniaOn <115379208+DaniaOn@users.noreply.github.com>
2023-02-15 19:41:14 -0600

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.handlebars", "./views/**/*.html", "./public/js/**/*.js", "**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
