76bf2edfdc505b2c3f130d298bd480e50190e5ec
DaniaOn <115379208+DaniaOn@users.noreply.github.com>
2023-02-15 19:39:00 -0600

    Delete controllers directory

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const foodRoutes = require('./food-routes');
-const userRoutes = require('./user-routes');
-
-router.use('/food', foodRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
